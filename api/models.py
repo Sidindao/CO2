3d42d5283198a36fd977407496c2039c97f53c8e
IBEN HABEL FADY <fady.iben-habel@etu.univ-lyon1.fr>
2025-05-12 20:31:44 +0200

    Adding AdminUser class

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Float, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -7,3 +7,10 @@ class EmissionCO2(Base):
     __tablename__ = "emissions_co2"
     mode_transport = Column(String, primary_key=True)
     emission_par_km = Column(Float, nullable=False)
+    
+class AdminUser(Base):
+    __tablename__ = "admin_users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
