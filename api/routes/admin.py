670e95e4c528273c6a5b083c3c1f4810bb4853f3
Hoogenbosch Lucca <lucca.hoogenbosch@etu.univ-lyon1.fr>
2025-05-20 16:21:30 +0200

    Fix: Catch serveur error for login

--- api/routes/admin.py
+++ api/routes/admin.py
@@ -10,11 +10,15 @@ router = APIRouter(prefix="/admin", tags=["admin"])
 
 @router.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(AdminUser).where(AdminUser.username == form_data.username))
+    try:
+        result = await db.execute(select(AdminUser).where(AdminUser.username == form_data.username))
+    except Exception as exc:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, \
+            detail=f"Erreur lors de la connexion a la table admin_users: {exc}") from exc
     user = result.scalars().first()
 
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-    
+
     access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
